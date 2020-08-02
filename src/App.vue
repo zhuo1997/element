f89da5db6eeb7d87de2bd9d61b2b3ae40901a152
mygit <1004002105@qq.com>
2020-08-02 11:40:07 +0800

    next

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.jpg">
+    <h4>永远相信美好的事情即将发生</h4>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
