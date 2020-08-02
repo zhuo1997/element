35092966687eb627828b422b6ac5794c3e38d09b
mygit <1004002105@qq.com>
2020-08-02 13:23:23 +0800

    sss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.jpg">
+    <!-- <img alt="Vue logo" src="./assets/logo.jpg"> -->
     <h4>永远相信美好的事情即将发生</h4>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
